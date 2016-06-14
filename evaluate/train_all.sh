ec1d7916f748dff740e0dea5c4dabd4673ec2e75
Kenta OONO <oono@preferred.jp>
2016-06-14 11:47:49 +0900

    Change the number of iterations

--- evaluate/train_all.sh
+++ evaluate/train_all.sh
@@ -2,20 +2,20 @@
 
 for predictor in inception-v3 alex vgg resnet-50
 do
-    python train_image.py -p ${predictor} -g -1 -b 2
+    python train_image.py -p ${predictor} -g -1 -b 2 -i 2
 done
 
 for predictor in deepspeech2 fc5
 do
-    python train_audio.py -p ${predictor} -g -1 -b 2
+    python train_audio.py -p ${predictor} -g -1 -b 2 -i 2
 done
 
 for predictor in c3d
 do
-    python train_movie.py -p ${predictor} -g -1 -b 2
+    python train_movie.py -p ${predictor} -g -1 -b 2 -i 2
 done
 
 for predictor in small-lstm big-lstm
 do
-    python train_text.py -p ${predictor} -g -1 -b 2
+    python train_text.py -p ${predictor} -g -1 -b 2 -i 2
 done
