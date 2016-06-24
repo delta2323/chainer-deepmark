1dedeee580edeb9410103c5d3e1116dd9601dfbd
Kenta OONO <oono@preferred.jp>
2016-06-24 09:23:01 +0900

    Foolow the naming convention of VGG team.
    Reference from https://gist.github.com/ksimonyan/211839e770f7b538e2d8
    I cannot find out the convention when the number of convolution layers in
    the group is 1 as the architectures published by VGG group have more than 1 layer
    for each layer group.
    For now we name conv1 and conv2 etc. instead of conv1_1 and conv2_1 for simplicity.

--- deepmark_chainer/net/vgg_d.py
+++ deepmark_chainer/net/vgg_d.py
@@ -16,19 +16,19 @@ class VGG_D(link.Chain):
 
     def __init__(self, use_cudnn=True):
         super(VGG_D, self).__init__(
-            conv1a=convolution_2d.Convolution2D(3, 64, 3, pad=1, use_cudnn=use_cudnn),
-            conv1b=convolution_2d.Convolution2D(64, 64, 3, pad=1, use_cudnn=use_cudnn),
-            conv2a=convolution_2d.Convolution2D(64, 128, 3, pad=1, use_cudnn=use_cudnn),
-            conv2b=convolution_2d.Convolution2D(128, 128, 3, pad=1, use_cudnn=use_cudnn),
-            conv3a=convolution_2d.Convolution2D(128, 256, 3, pad=1, use_cudnn=use_cudnn),
-            conv3b=convolution_2d.Convolution2D(256, 256, 3, pad=1, use_cudnn=use_cudnn),
-            conv3c=convolution_2d.Convolution2D(256, 256, 3, pad=1, use_cudnn=use_cudnn),
-            conv4a=convolution_2d.Convolution2D(256, 512, 3, pad=1, use_cudnn=use_cudnn),
-            conv4b=convolution_2d.Convolution2D(512, 512, 3, pad=1, use_cudnn=use_cudnn),
-            conv4c=convolution_2d.Convolution2D(512, 512, 3, pad=1, use_cudnn=use_cudnn),
-            conv5a=convolution_2d.Convolution2D(512, 512, 3, pad=1, use_cudnn=use_cudnn),
-            conv5b=convolution_2d.Convolution2D(512, 512, 3, pad=1, use_cudnn=use_cudnn),
-            conv5c=convolution_2d.Convolution2D(512, 512, 3, pad=1, use_cudnn=use_cudnn),
+            conv1_1=convolution_2d.Convolution2D(3, 64, 3, pad=1, use_cudnn=use_cudnn),
+            conv1_2=convolution_2d.Convolution2D(64, 64, 3, pad=1, use_cudnn=use_cudnn),
+            conv2_1=convolution_2d.Convolution2D(64, 128, 3, pad=1, use_cudnn=use_cudnn),
+            conv2_2=convolution_2d.Convolution2D(128, 128, 3, pad=1, use_cudnn=use_cudnn),
+            conv3_1=convolution_2d.Convolution2D(128, 256, 3, pad=1, use_cudnn=use_cudnn),
+            conv3_2=convolution_2d.Convolution2D(256, 256, 3, pad=1, use_cudnn=use_cudnn),
+            conv3_3=convolution_2d.Convolution2D(256, 256, 3, pad=1, use_cudnn=use_cudnn),
+            conv4_1=convolution_2d.Convolution2D(256, 512, 3, pad=1, use_cudnn=use_cudnn),
+            conv4_2=convolution_2d.Convolution2D(512, 512, 3, pad=1, use_cudnn=use_cudnn),
+            conv4_3=convolution_2d.Convolution2D(512, 512, 3, pad=1, use_cudnn=use_cudnn),
+            conv5_1=convolution_2d.Convolution2D(512, 512, 3, pad=1, use_cudnn=use_cudnn),
+            conv5_2=convolution_2d.Convolution2D(512, 512, 3, pad=1, use_cudnn=use_cudnn),
+            conv5_3=convolution_2d.Convolution2D(512, 512, 3, pad=1, use_cudnn=use_cudnn),
             fc6=linear.Linear(512 * 7 * 7, 4096),
             fc7=linear.Linear(4096, 4096),
             fc8=linear.Linear(4096, 1000)
@@ -36,23 +36,23 @@ class VGG_D(link.Chain):
         self.use_cudnn = use_cudnn
 
     def __call__(self, x):
-        h = relu.relu(self.conv1a(x), self.use_cudnn)
-        h = relu.relu(self.conv1b(h), self.use_cudnn)
+        h = relu.relu(self.conv1_1(x), self.use_cudnn)
+        h = relu.relu(self.conv1_2(h), self.use_cudnn)
         h = max_pooling_2d.max_pooling_2d(h, 2, stride=2, use_cudnn=self.use_cudnn)
-        h = relu.relu(self.conv2a(h), self.use_cudnn)
-        h = relu.relu(self.conv2b(h), self.use_cudnn)
+        h = relu.relu(self.conv2_1(h), self.use_cudnn)
+        h = relu.relu(self.conv2_2(h), self.use_cudnn)
         h = max_pooling_2d.max_pooling_2d(h, 2, stride=2, use_cudnn=self.use_cudnn)
-        h = relu.relu(self.conv3a(h), self.use_cudnn)
-        h = relu.relu(self.conv3b(h), self.use_cudnn)
-        h = relu.relu(self.conv3c(h), self.use_cudnn)
+        h = relu.relu(self.conv3_1(h), self.use_cudnn)
+        h = relu.relu(self.conv3_2(h), self.use_cudnn)
+        h = relu.relu(self.conv3_3(h), self.use_cudnn)
         h = max_pooling_2d.max_pooling_2d(h, 2, stride=2, use_cudnn=self.use_cudnn)
-        h = relu.relu(self.conv4a(h), self.use_cudnn)
-        h = relu.relu(self.conv4b(h), self.use_cudnn)
-        h = relu.relu(self.conv4c(h), self.use_cudnn)
+        h = relu.relu(self.conv4_1(h), self.use_cudnn)
+        h = relu.relu(self.conv4_2(h), self.use_cudnn)
+        h = relu.relu(self.conv4_3(h), self.use_cudnn)
         h = max_pooling_2d.max_pooling_2d(h, 2, stride=2, use_cudnn=self.use_cudnn)
-        h = relu.relu(self.conv5a(h), self.use_cudnn)
-        h = relu.relu(self.conv5b(h), self.use_cudnn)
-        h = relu.relu(self.conv5c(h), self.use_cudnn)
+        h = relu.relu(self.conv5_1(h), self.use_cudnn)
+        h = relu.relu(self.conv5_2(h), self.use_cudnn)
+        h = relu.relu(self.conv5_3(h), self.use_cudnn)
         h = max_pooling_2d.max_pooling_2d(h, 2, stride=2, use_cudnn=self.use_cudnn)
         h = self.fc6(h)
         h = self.fc7(h)
